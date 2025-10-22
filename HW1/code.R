8c1ac6d554a0c7b54ad40cf1046daf05d0b5481b
André Venâncio <andre.venancio@alu.ufc.br>
2025-10-21 22:55:13 -0300

    Comeco questao 2

--- HW1/code.R
+++ HW1/code.R
@@ -44,8 +44,28 @@ length(gas_above_limit) # 11 out of 73 days with gas emission above the limit. i
 ######################   QUESTION 2    ######################
 #############################################################
 
-# fica pra vc andre
 
+# our data
+idade <- c(28, 34, 46, 26, 37, 29, 51, 31, 39, 43, 58, 44, 25, 23, 52, 42, 48, 33, 38, 46)
+nacionalidade <- c(Italiana, Inglesa, Belga, Espanhola, Italiana, Espanhola, Francesa, Belga, Italiana, Italiana, Italiana, Inglesa, Francesa, Espanhola, Italiana, Alemã, Francesa, Italiana, Alemã, Italiana)
+renda <- c(2.3, 1.6, 1.2, 0.9, 2.1, 1.6, 1.8, 1.4, 1.2, 2.8, 3.4, 2.7, 1.6, 1.2, 1.1, 2.5, 2.0, 1.7, 2.1, 3.2)
+xp <- c(2, 8, 21, 1, 15, 3, 28, 5, 13, 20, 32, 23, 1, 0, 29, 18, 19, 7, 12, 23)
+
+#1.
+media_idade <- mean(idade)
+media_renda <- mean(renda)
+media_xp <- mean(xp)
+
+mediana_idade <- median(idade)
+mediana_renda <- median(renda)
+mediana_xp <- median(xp)
+
+dp_idade <- sd(idade)
+dp_renda <- sd(renda)
+dp_xp <- sd(xp)
+
+#2.
+df <- data.frame(idade, nacionalidade, renda, xp)
 
 
 
